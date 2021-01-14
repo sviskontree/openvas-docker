435d9bb8f5919ed52f38915d66d90cf50851584c
sviskontree <pge@localhost>
2021-01-14 20:20:33 +0100

    Split db and openvas container and added some checks to the startup script

--- README.md
+++ README.md
@@ -1,4 +1,49 @@
-TODO
+Two docker containers to run Openvas, the build includes everything needed for it to run.
 
+Usage
+-----
+
+The easiest way to run it is with docker-compose. The below example will create one openvas containers that runs gui + scanning, it creates persistence in the same directory as ran from. A separate postgresql container with the user openvas and the db gvmd aswell as the option POSTGRES_HOST_AUTH_METHOD is run. These options are currently a requirement for it to run.
+
+```
+version: '3.2'
+
+services:
+  openvas:
+    image: sviskontree/openvas:v20.8.0
+    volumes:
+      - ./gvmd:/usr/local/var/lib/gvm/gvmd
+    ports:
+      - 443:443
+    depends_on:
+      - psql
+
+  psql:
+    image: sviskontree/openvas-postgresql:v20.8.0
+    restart: always
+    volumes:
+      - ./postgres:/var/lib/postgresql/data
+    environment:
+      POSTGRES_USER: openvas
+      POSTGRES_DB: gvmd
+      POSTGRES_HOST_AUTH_METHOD: trust
+```
+
+Environment variables
+-----
+
+Name     | Description | Default
+---------|------------|-----------
+OPENVAS_ADMIN_USER | admin user used for login to webgui | admin
+OPENVAS_ADMIN_PW | password for the admin user | admin
+POSTGRES_USER | user to use when connecting with the postgresql database (should not be changed) | openvas
+POSTGRES_SERVER | postgres server name/ip | psql
+POSTGRES_PORT | postgres port to connect to | 5432
+
+For options available to the postgresql container see [postgres](https://hub.docker.com/_/postgres) on dockerhub
+
+Extra
+------
 Openvas made by [Greenbone](https://github.com/greenbone/)
+
 Heavily inspired by Mikesplains [openvas-docker](https://github.com/mikesplain/openvas-docker)
---
+++ files/do_it.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+mkdir -p /var/run/{postgresql,redis,ospd,gvmd}
+chown openvas /var/run/{ospd,gvmd,postgresql}
+
+sleep 4
+sudo -u openvas nohup socat UNIX-LISTEN:/var/run/postgresql/.s.PGSQL.5432,fork tcp:${POSTGRES_SERVER:-psql}:${POSTGRES_PORT:-5432} &
+sudo -u openvas psql -U ${POSTGRES_USER:-openvas} -h /var/run/postgresql/ -d gvmd -c "CREATE EXTENSION \"uuid-ossp\";"
+sudo -u openvas psql -U ${POSTGRES_USER:-openvas} -h /var/run/postgresql/ -d gvmd -c "CREATE EXTENSION \"pgcrypto\";"
+sudo -u openvas psql -U ${POSTGRES_USER:-openvas} -h /var/run/postgresql/ -d gvmd -c "CREATE ROLE dba WITH SUPERUSER NOINHERIT;"
+sudo -u openvas psql -U ${POSTGRES_USER:-openvas} -h /var/run/postgresql/ -d gvmd -c "GRANT dba TO openvas;"
+
+sleep 8
+sudo -u openvas gvmd --migrate
+
+redis-server /etc/redis/redis.conf
+echo "Testing redis status..."
+X="$(redis-cli -s /var/run/redis/redis.sock ping)"
+while  [ "${X}" != "PONG" ]; do
+        echo "Redis not yet ready..."
+        sleep 1
+        X="$(redis-cli -s /var/run/redis/redis.sock ping)"
+done
+echo "Redis ready."
+
+if [ ! -f /usr/local/var/lib/gvm/CA/servercert.pem ]; then
+        sudo -u openvas gvm-manage-certs -a
+fi
+
+echo "Gmvd will now rebuild-scap data, it will take a while"
+sudo -u openvas gvmd --rebuild-scap
+
+cd /tmp/
+echo "Starting ospd-openvas"
+sudo -u openvas nohup ospd-openvas -f -c /usr/local/var/lib/gvm/CA/servercert.pem --ca-file /usr/local/var/lib/gvm/CA/cacert.pem -k /usr/local/var/lib/gvm/private/CA/serverkey.pem -u /var/run/ospd/ospd.sock --pid-file /var/run/ospd/ospd.pid &
+sleep 12
+echo "Starting gvmd and gsad"
+sudo -u openvas nohup gvmd -f -c /var/run/gvmd/gvmd.sock &
+sudo -u openvas nohup gsad -f --munix-socket=/var/run/gvmd/gvmd.sock &
+
+sleep 8
+mkdir -p /usr/local/var/lib/gvm/gvmd/report_formats/ && chmod 755  /usr/local/var/lib/gvm/gvmd/report_formats/ && chown -R openvas /usr/local/var/lib/gvm/gvmd/
+
+if sudo -u openvas gvmd --get-users | grep ${OPENVAS_ADMIN_USER}; then
+        sudo -u openvas gvmd --create-user=${OPENVAS_ADMIN_USER:-admin} --password=${OPENVAS_ADMIN_PW:-admin}
+        sudo -u openvas gvmd --modify-setting 78eceaec-3385-11ea-b237-28d24461215b --value $(sudo -u openvas gvmd --get-users --verbose | awk '{print $2}')
+fi
+
+sudo -u openvas gvmd --rebuild
+echo -e "\nOpenvas should now be starting\nIf this is the first time you are running, it will be a while before import of all data is done\n"
+tail -f /usr/local/var/log/gvm/*
